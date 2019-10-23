1c104666dbca04b0e02f15fed36e76b4b1dea1f7
Keegan Nair <keegan.nair@hotmail.com>
2019-10-23 22:00:34 +1100

    added some notes and added a while loop to repeat the alert if the number is too low or too high

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,41 @@
 //generate random password
 function generate(){
+    var size = undefined;
+
 
     //set password length
-    var size = prompt("How many characters to do want in the password?");
+    while (size == undefined) {
+        var userInput = parseInt(prompt("How many characters to do want in the password?"));
 
-    if(size < 8){
-        alert("You must have more than 8 characters in a password");
-    } else if(size > 128){
-        alert("Password must be less than 128 characters");
-    }//else
+        if (userInput < 8) {
+            alert("You must have more than 8 characters in a password");
+        } else if (userInput > 128) {
+            alert("Password must be less than 128 characters");
+        } else {
+            size = userInput;
+        }
+    }
+    
+
+    //need to add a prompt asking user if they want a special character.
+    //need to make sure that the special character is included into the password.
+    //need to change the values
+    //need to add if statement where it checks if the user wants to add a special character.
 
     //possible password values
-    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
-    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    var numbers = "1234567890";
-    var specialChar = "!@#$%^&*()_+";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890";
+    //var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    //var numbers = "1234567890";
+    //var specialChar = "!@#$%^&*()_+";
 
     var password = "";
 
     //create for loop to choose password characters
-    for(var i = 0; i <= size; i++){
+    for(var i = 0; i < size; i++){
         password = password + upperCase.charAt(Math.floor(Math.random() * Math.floor(upperCase.length - 1)));
-        password = password + lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length - 1)));
-        password = password + numbers.charAt(Math.floor(Math.random() * Math.floor(numbers.length - 1)));
-        password = password + specialChar.charAt(Math.floor(Math.random() * Math.floor(specialChar.length - 1)));
+       // password = password + lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length - 1)));
+       // password = password + numbers.charAt(Math.floor(Math.random() * Math.floor(numbers.length - 1)));
+       // password = password + specialChar.charAt(Math.floor(Math.random() * Math.floor(specialChar.length - 1)));
 
     }
 
